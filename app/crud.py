077f2b76ce1b97a1ea068b2b383485e20bc513d0
Samuel3Shin <samuel3.shin@gmail.com>
2023-03-21 14:22:29 -0700

    removed comment

--- app/crud.py
+++ app/crud.py
@@ -6,7 +6,6 @@ def get_user(db: Session, username: str):
     return db.query(models.User).filter(models.User.username == username).first()
 
 def create_user(db: Session, user: schemas.UserCreate):
-    # hashed_password = pwd_context.hash(user.password)
     db_user = models.User(username=user.username, password=user.password)
     db.add(db_user)
     db.commit()
