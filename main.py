249967d7f37569721bb31bf1ca84f5ae0b900b60
Samuel3Shin <samuel3.shin@gmail.com>
2023-03-22 11:26:30 -0700

    modularized routers

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
-from app import api, models
+from app import models
 from app.database import engine
+from app.routers import users, tasks
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(api.router)
+app.include_router(users.router, prefix="/users", tags=["users"])
+app.include_router(tasks.router, prefix="/tasks", tags=["tasks"])
